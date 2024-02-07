7dbea54091cd60aa4bc1c398344a2b42b97cac97
va-lang <vanessabedzra@gmail.com>
2024-02-06 21:44:17 -0800

    comment addition

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,7 +4,17 @@
 
 
 class Square:
+    """A class that represents a square"""
+
     def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
@@ -27,4 +37,9 @@ class Square:
         self.__size = value
 
     def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
         return (self.__size ** 2)
